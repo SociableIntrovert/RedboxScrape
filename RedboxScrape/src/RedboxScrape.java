d26dca6ca037336e5f9931820334d447f5632b7c
ryon <ryon@WarMachine>
2016-09-25 10:27:09 -0500

    update

--- RedboxScrape/src/RedboxScrape.java
+++ RedboxScrape/src/RedboxScrape.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -10,22 +11,22 @@ import org.openqa.selenium.WebElement;
 public class RedboxScrape {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver", "/home/ryon/Downloads/geckodriver");
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Ryon\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//driver.manage().window().maximize();
 		String baseurl = "http://www.redbox.com/locations/state";
 		driver.get(baseurl);
 		
 		List<WebElement> states = driver.findElements(By.xpath("//div[@class='store-column']/a"));
-		File outputFile = new File("/home/ryon/Desktop/records.txt");
+		File outputFile = new File("C:\\Users\\Ryon\\Desktop\\RedboxScrape.txt");
 		BufferedWriter output = null;
-		
 		try{
 			output = new BufferedWriter(new FileWriter(outputFile, true));
 			for(int statecount = 0; statecount < states.size(); statecount++){
 				List<WebElement> statesRefreshed = driver.findElements(By.xpath("//div[@class='store-column']/a"));
 				driver.get(statesRefreshed.get(statecount).getAttribute("href"));
 				List<WebElement> cities = driver.findElements(By.xpath("//div[@class='store-column']/a"));
-				for(int citycount = 0; citycount < cities.size(); citycount++){
+				for(int citycount = 0; citycount <  cities.size(); citycount++){
 					List<WebElement> citiesRefreshed = driver.findElements(By.xpath("//div[@class='store-column']/a"));
 					driver.get(citiesRefreshed.get(citycount).getAttribute("href"));
 					List<WebElement> zips = driver.findElements(By.xpath("//div[@class='store-column']/a"));
@@ -40,16 +41,16 @@ public class RedboxScrape {
 							output.flush();
 							System.out.print(companyName.get(sitecount).getText() + " ");
 							System.out.println(location.get(sitecount).getText().replaceAll("\n", "|"));
-						}//End 3rd inner for
+						}
+						 
 						driver.navigate().back();
-					}//End 2nd inner for
+					}//End zip loop
 					driver.navigate().back();
-				}//End inner for
+				}//End city loop
 				driver.navigate().back();
-			}//End outer for
+			}//End state loop
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
-
 	}//End main
 }//End class
